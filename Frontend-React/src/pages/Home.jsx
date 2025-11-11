17b5d4f62dd55e6f089bf34ca85818de4dbfdbab
Zoe <zoepereyra026@gmail.com>
2025-11-11 00:53:20 -0300

    Modificar el Home.jsx

--- Frontend-React/src/pages/Home.jsx
+++ Frontend-React/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../components/NavBar';
+import Navbar from '../components/navbar';
 import Descripcion from '../components/Descripcion';
 import CarrucelProductos from '../components/CarruselProductos';
 import Footer from '../components/Footer';

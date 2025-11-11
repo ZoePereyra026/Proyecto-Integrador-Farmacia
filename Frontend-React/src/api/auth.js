9d21f272d40d90004ec5db630061cb0584063588
Zoe <zoepereyra026@gmail.com>
2025-11-11 01:19:03 -0300

    Agregar API_URL actual

--- Frontend-React/src/api/auth.js
+++ Frontend-React/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:3000/api/usuarios";
+const API_URL = `${import.meta.env.VITE_BACKEND_URL}/api/usuarios`;
 
 export const registerUser = async (id, username, email, password) => {
   try {
